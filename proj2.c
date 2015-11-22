2bc59d93f2022e8f8c6815b0b832e12df8bb9bc8
Frantisek Farkas <frantisekfarkas93@gmail.com>
2015-11-22 20:27:29 +0100

    implemented functions for calculating log with taylor polynomial and continued fractions

--- proj2.c
+++ proj2.c
@@ -7,21 +7,60 @@
 
 double taylor_log(double x, unsigned int n){
 	double result;
+	double prvy_clen;
+	double dalsi_clen;	
 	
-	return result;	
+	if(x >= 1){
 	
-}
-
-double cf_log(double x, unsigned int n){
+	prvy_clen = (x-1)/x;
+	result = prvy_clen;	
+	double h1 = prvy_clen;
 	
-	double result;
 	
+	for(double i = 2; i <= n; i++){
+		
+		dalsi_clen = prvy_clen * h1 * ((i-1)/(i));
+		prvy_clen = dalsi_clen;
+		result += dalsi_clen;
+	}
+	
+	
+	}else if(x < 1 && x > 0){
+		prvy_clen = 1 - x;	
+		result = prvy_clen;
+		double h2 = prvy_clen;
+		
+		for(int i = 2; i <= n; i++){
+		
+		dalsi_clen = prvy_clen * ((h2)*(i-1))/i;
+		prvy_clen = dalsi_clen;
+		result += dalsi_clen;
+		
+		
+		}
+		result = result * (-1);
+	}
+	else{
+		
+		return NAN;
+	}
 	return result;
-
 }
 
-double cfrag_log(double x, unsigned int n){
+
+double cfrac_log(double x, unsigned int n){
 	double result;
+	double clen = 0;
+	double z = (x-1)/(1+x);
+	double z2 = z*z;
+	
+	for(double i = n; i > 1; i--){
+		
+		clen = ((i-1)*(i-1)* z2 )/ ((2*i - 1) - clen);
+	}
+	
+	result = (2*z) / (1 - clen);
+	
 	
 	return result;
 }
@@ -39,6 +78,7 @@ bool is_number(char *str){
 	
 }
 int main(int argc, char *argv[]){
+	/*
 		double min, max, eps;	
 	
 	if(!strcmp(argv[1], "--iter")){
@@ -53,12 +93,25 @@ int main(int argc, char *argv[]){
 		printf("%s", argv[1]);
 	
 	}
+	* */
+	
+	
+	double result_log = log(1000000000);
+	printf("log: %f\n", result_log);
+	
+	double result = taylor_log(1000000000, 1000000); 
+	printf("taylor: %f\n", result);
+	
+	double result2 = cfrac_log(1000000000, 1000000);
+	printf("cf: %f\n", result2);
+	
 	
 	return 1;
 	}
 
 
 	
+
 
 
 
